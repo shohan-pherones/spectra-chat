c080c69459fc84194511d48bf12afc4f4039b7ab
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-23 18:40:38 +0600

    pusher messages updated

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,7 @@
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import prisma from '@/app/libs/db';
 import { NextResponse } from 'next/server';
+import { pusherServer } from '@/app/libs/pusher';
 
 export async function POST(request: Request) {
   try {
@@ -55,6 +56,18 @@ export async function POST(request: Request) {
           },
         },
       },
+    });
+
+    await pusherServer.trigger(conversationId, 'messages:new', newMessage);
+
+    const lastMessage =
+      updatedConversation.messages[updatedConversation.messages.length - 1];
+
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, 'conversation:update', {
+        id: conversationId,
+        messages: [lastMessage],
+      });
     });
 
     return NextResponse.json(newMessage);
